be21bc4a1774e476e1a97e881b4150314d3ba3ee
juliabrchrdt <63443884+juliabrchrdt@users.noreply.github.com>
2020-12-01 12:47:23 +0100

    Fontsize Anpassung

--- src/pages/ausstattung.js
+++ src/pages/ausstattung.js
@@ -7,7 +7,7 @@ export default () => (
   <Layout>
     <Header alt={"header"}  filename={"ausstattung.jpg"} title={"Ausstattung & Specials"}  styling={{height: "111%"}}/>
     <TextBox
-      title={"Gin Voyager Bar"}
+      title={"Cigar Voyager Bar"}
       text={`Eine Zigarre bildet zusammen mit dem passenden Drink ein Sinnbild von Luxus und Genuss – somit wird sie mit besonderen Anlässen in Verbindung gebracht.
 
 Deshalb präsentieren wir Ihnen unsere Alfred & Son Cigar-Bar. Das Ensemble, bestehend aus aufwendig restaurierten Überseekoffern aus den 20er Jahren, lässt Raum für bis zu zwanzig erlesenen Bränden, welche von uns korrespondierend zu den Zigarren ausgewählt werden. Die klassische Kombination von Zigarren und diversen Whiskys ist die Basis der Digestif-Bar und wird durch weitere fassgelagerte Brände abgerundet. Das Herzstück der Cigar-Bar charakterisiert der massive Zigarrentisch mit einem gepflegten Humidor.`}
